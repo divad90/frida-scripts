748b66be23d9456e6dbf932318645fb513197656
divad90 <57325452+divad90@users.noreply.github.com>
2023-03-14 14:43:23 -0600

    Add files via upload

---
+++ decrypt-and-show.js
@@ -0,0 +1,23 @@
+/*
+It decrypts the password using the same functions used to encrypt.
+Link: https://github.com/OWASP/owasp-mastg/tree/master/Crackmes/Android/Level_01
+*/
+Java.perform(()=>{
+
+    //-- base64decode --
+    const base64 = Java.use("android.util.Base64");
+    var arrayofbyte = base64.decode("5UJiFctbmgbDoLXmpL12mkno8HT4Lv8dlat8FxR2GOc=",0);
+
+    //-- b function --
+    const bfunclass = Java.use("sg.vantagepoint.uncrackable1.a");
+    var enckey = bfunclass.b("8d127684cbc37c17616d806cf50473cc");
+
+    //-- a function --
+    const afunclass = Java.use("sg.vantagepoint.a.a");
+    var decryptedarray = afunclass.a(enckey, arrayofbyte);
+
+    //-- convert to string and show --
+    const strclass = Java.use("java.lang.String");
+    var decryptedstr = strclass.$new(decryptedarray);
+    console.log(decryptedstr);
+});
