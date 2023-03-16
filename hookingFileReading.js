3736e17bbfdd09f201bb9e94331fa19ffc05b2fd
divad90 <57325452+divad90@users.noreply.github.com>
2023-03-16 11:40:37 -0600

    Add files via upload

---
+++ hookingFileReading.js
@@ -0,0 +1,28 @@
+/*
+It hooks the CreateFileA function
+*/
+
+const createfileaddr = Module.getExportByName("kernel32.dll", "CreateFileA");
+
+console.log("-- createfile addr: " + createfileaddr);
+
+
+Interceptor.attach(createfileaddr, {
+    onEnter: function(args){
+        console.log("--entered createfile--");
+        console.log("--lpFileName: " + args[0].readUtf8String());
+        console.log("--dwDesiredAccess: " + args[1]);
+    },
+
+    onLeave: function(retval){
+        //retval.replace(1);
+        console.log("--exited createfile--");
+        console.log("-- createfile retval: " + retval);
+        
+    }
+});
+
+
+
+
+
